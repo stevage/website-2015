53b9811bd22307254b9c30ae051c0158e85e8367
maggie mu <maggie.mu2010@gmail.com>
2015-07-01 10:38:41 +1000

    add regions and past challenges on the home page

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,16 @@
 
+
+$( document ).ready(function() {
+
+        function bindEvent() {
+            $(".event-region-text").hover(function () {
+                $(this).parent().find('.event-region').hover();
+            });
+        }
+
+        bindEvent();
+
+    }
+);
+
+
