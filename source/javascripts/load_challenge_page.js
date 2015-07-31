8eb14fbed34641dccf18ab1535d12e0411a3de9d
Sam McKendrick <smckend@thoughtworks.com>
2015-07-31 14:52:27 +1000

    Added content to challenges

--- source/javascripts/load_challenge_page.js
+++ source/javascripts/load_challenge_page.js
@@ -6,13 +6,21 @@ $( document ).ready(function() {
 			if (challenge.id == $("#challenge-id").text()) {
 				panel.find("div.banner h2.name").text(challenge.name);
 				panel.find("div.banner h3.city").text(challenge.city);
-				panel.find("div.challenge-title img.sponsor").attr("src", challenge.organisationImg);
-				panel.find("div.challenge-title img.sponsor").attr("alt", challenge.organisation);
+				if (challenge.organisationImg) {
+					panel.find("div.challenge-title img.sponsor").attr("src", challenge.organisationImg);
+					panel.find("div.challenge-title img.sponsor").attr("alt", challenge.organisation);
+				}
+				else {
+					panel.find("div.challenge-title img.sponsor").attr("src", "images/healthhack-logo/HH_Logo.svg");
+					panel.find("div.challenge-title img.sponsor").attr("alt", "Health-Hack");
+				}
+		//	panel.find("div.challenge-title img.sponsor").attr("src", challenge.organisationImg);
+	  //	panel.find("div.challenge-title img.sponsor").attr("alt", challenge.organisation);
 		    panel.find("div.organiser-details p").text("Presented by " + challenge.presenters);
 				panel.find("div.organiser-details a").text(challenge.organisation);
 				panel.find("div.organiser-details a").attr("href", challenge.organisationLink);
 				panel.find("div.challenge-data p.description").html(challenge.description);
-			//	panel.find("div.challenge-data a.team").attr("href", challenge.teamLink);
+		//	panel.find("div.challenge-data a.team").attr("href", challenge.teamLink);
 				if (challenge.team) {
 					var demoPanel = $("<a></a>");
 					demoPanel.html(
