51fd836c29839b567bbc222784599cd32e91604b
minillinim <michael.imelfort@gmail.com>
2015-08-08 01:53:35 +1000

    Challenges ported to new way of being

--- source/javascripts/load_challenges.js
+++ source/javascripts/load_challenges.js
@@ -1,36 +1,90 @@
 $( document ).ready(function() {
 	$.get("data/challenges.json", function(data) {
-		var template = $("#challenge-list div.list div.challenge-row");
+		var template = $("div.challenge-row");
 		$.each (data.challenges, function(index, challenge) {
 			var panel = template.clone();
+			var custom_row_id = "challenge-row-"+index;
+			panel.attr("id", custom_row_id);
+
 			if (challenge.winner) {
-				var winnerPanel = $("<span></span>");
-				winnerPanel.html(
-					"<span class=\"glyphicon glyphicon-star\"></span>" + " " +
-                    challenge.winner);
-				winnerPanel.addClass("challenge-winner-label");
-				winnerPanel.addClass("clearfix");
-                winnerPanel.prependTo(panel.find("div:first-child"));
+				var winner_panel = $("<span>", { class: "clearfix, challenge-winner-label",
+					                             html: "<span class=\"glyphicon glyphicon-star\"></span>" + " " + challenge.winner });
+                panel.find("#"+custom_row_id).prepend(winner_panel);
 			}
-			panel.find("h3.name").text(challenge.name);
+
+			panel.find("h2.name").text(challenge.name);
 			panel.find("p.city span").text(challenge.city);
-			panel.find("p.city a").text(challenge.organisation);
+
+			if (challenge.organisationImg) {
+				panel.find("p.city a").text(challenge.organisation);
+			} else {
+				panel.find("p.city a").text("Supported by Health Hack");
+			}
 			panel.find("p.city a").attr("href", challenge.organisationLink);
 			panel.find("p.summary").text(challenge.summary);
-			if (challenge.workingDemoLink) {
-				var demoPanel = $("<a></a>");
-				demoPanel.html(
-					"<button type=\"submit\" class=\"btn-primary btn grey-btn\">WORKING DEMO</button>");
-				demoPanel.addClass("workingDemoLink");
-				demoPanel.attr("href", challenge.workingDemoLink);
-				demoPanel.prependTo(panel.find("div.challenge-links"));
+
+			// details
+			var custom_id = "challenge-page-container-"+index;
+			panel.find("#challenge-page-container").attr("id", custom_id);
+
+			if (challenge.organisationImg) {
+				panel.find("div.challenge-title img.sponsor").attr("src", 'images/' + challenge.organisationImg);
+				panel.find("div.challenge-title img.sponsor").attr("alt", challenge.organisation);
 			}
-			//panel.find("a.workingDemoLink").attr("href", challenge.workingDemoLink);
-			panel.find("a.detailsLink").attr("href", challenge.detailsLink);
-			panel.appendTo("#challenge-list div.list");
+			else {
+				panel.find("div.challenge-title img.sponsor").attr("src", "images/healthhack-logo/HH_Logo.svg");
+				panel.find("div.challenge-title img.sponsor").attr("alt", "Health-Hack");
+			}
+
+			panel.find("div.organiser-details p").text("Presented by " + challenge.presenters);
+			panel.find("div.organiser-details a").text(challenge.organisation);
+			panel.find("div.organiser-details a").attr("href", challenge.organisationLink);
+
+			var desc_text = "";
+			$.each(challenge.description, function(index, desc_line) {
+				desc_text += desc_line + "<br><br>";
+			});
+			panel.find("div.challenge-data p.description").html(desc_text);
+			
+			//	panel.find("div.challenge-data a.team").attr("href", challenge.teamLink);
+			if (challenge.team) {
+				var team_panel = $("<a>", { class: "team", html: "<b>VIEW TEAM INFORMATION</b>", "href": challenge.teamLink });
+				panel.find("div.details-panel").append(team_panel);
+			}
+
+			// Add working demo button
+			if (challenge.workingDemoLink) {
+				var demo_panel = $("<a>", { class: "workingDemoLink", "href": challenge.workingDemoLink });
+				var demo_button = $("<button>", { class: "btn-primary btn grey-btn", html: "WORKING DEMO" });
+				demo_panel.append(demo_button);
+				panel.find("div.challenge-links").append(demo_panel);
+			}
+
+			// show / hide details buttons
+			var details_panel = $("<div>", { class: "detailsLink"});
+			var show_more_button = $("<button>", { class: "btn-primary btn white-btn", id: "show-more-"+index, html: "DETAILS" });
+			var show_less_button = $("<button>", { class: "hide btn-primary btn white-btn", id: "show-less-"+index, html: "HIDE DETAILS" });
+			details_panel.append(show_more_button);
+			details_panel.append(show_less_button);
+			panel.find("div.challenge-links").append(details_panel);
+
+		    show_more_button.click(function () {
+		        $('#show-more-'+index).addClass("hide");
+		        $('#show-less-'+index).removeClass("hide");
+		        $('#'+custom_id).fadeIn("slow");
+		    });
+
+		    show_less_button.click(function () {
+		        $('#show-less-'+index).addClass("hide");
+		        $('#show-more-'+index).removeClass("hide");    
+		        $('#'+custom_id).fadeOut("slow");
+		    });
+
+			$("#challenge-list div.list").append(panel);
 		});
 
-		$("#challenge-list div.list div.challenge-row")[0].remove();
+		$("#challenge-row-template").remove();
+
 	}).always(function() {
 	      var monkeyList = new List('challenge-list', {
 	      valueNames: [ 'name', 'city', 'description', 'hightlight' ],
